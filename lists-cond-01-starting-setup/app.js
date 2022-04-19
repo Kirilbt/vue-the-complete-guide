550ddda69b9c75e83764e61d9431432daf60edcd
Kiril Tucker <kiril.tucker@gmail.com>
2022-04-19 16:23:57 +0200

    Lists & Keys

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -9,6 +9,9 @@ const app = Vue.createApp({
   methods: {
     addGoal() {
       this.goals.push(this.enteredGoalValue)
+    },
+    removeGoal(index) {
+      this.goals.splice(index, 1)
     }
   }
 });
