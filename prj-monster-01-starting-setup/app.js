8ba4a7b109148e66b0bbbe39722037d22970d157
Kiril Tucker <kiril.tucker@gmail.com>
2022-04-27 15:52:37 +0200

    Finishing the Core Functionality

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -13,9 +13,15 @@ const app = Vue.createApp({
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: ' 0%' }
+      }
       return {width: this.monsterHealth + '%'}
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: ' 0%' }
+      }
       return {width: this.playerHealth + '%'}
     },
     mayUseSpecialAttack() {
@@ -68,6 +74,15 @@ const app = Vue.createApp({
         this.playerHealth += healValue
       }
       this.attackPlayer()
+    },
+    startGame() {
+      this.playerHealth = 100
+      this.monsterHealth = 100
+      this.currentRound = 0
+      this.winner = null
+    },
+    surrender() {
+      this.winner = 'monster'
     }
   }
 })
