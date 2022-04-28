320ae68866e383a9293b8e45ae1cd9500f638658
Kiril Tucker <kiril.tucker@gmail.com>
2022-04-28 09:14:23 +0200

    Understanding Templates

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -16,3 +16,14 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+
+const app2 = Vue.createApp({
+  data() {
+    return {
+      favoriteSport: 'Football',
+    }
+  }
+})
+
+app2.mount('#app2')
