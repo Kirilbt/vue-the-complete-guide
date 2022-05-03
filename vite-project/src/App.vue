3dc1559bae3ab4b5d62286314e3bdc889ec9e2e5
Kiril Tucker <kiril.tucker@gmail.com>
2022-05-03 17:11:07 +0200

    Demo: Adding Components & Connecting Them

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -3,6 +3,7 @@
     <header>
       <h1>My Friends</h1>
     </header>
+    <new-friend @add-contact="addContact"></new-friend>
     <ul>
       <friend-contact
         v-for="friend in friends"
@@ -44,6 +45,16 @@
       toggleFavoriteStatus(friendId) {
         const identifiedFriend = this.friends.find(friend => friend.id === friendId)
         identifiedFriend.isFavorite = !identifiedFriend.isFavorite
+      },
+      addContact(name, phone, email) {
+        const newFriendContact = {
+          id: new Date().toISOString(),
+          name: name,
+          phone: phone,
+          email: email,
+          isFavorite: false
+        }
+        this.friends.unshift(newFriendContact)
       }
     }
   }
@@ -82,7 +93,8 @@
     list-style: none;
   }
 
-  #app li {
+  #app li,
+  #app form {
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
     margin: 1rem auto;
     border-radius: 10px;
@@ -114,5 +126,21 @@
     background-color: #ec3169;
     border-color: #ec3169;
     box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+  }
+
+  #app input {
+  font: inherit;
+  padding: 0.15rem;
+  }
+
+  #app label {
+    font-weight: bold;
+    margin-right: 1rem;
+    width: 7rem;
+    display: inline-block;
+  }
+
+  #app form div {
+    margin: 1rem 0;
   }
 </style>
