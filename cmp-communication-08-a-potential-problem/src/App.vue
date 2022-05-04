91c8c026bba53fc603d2c27f1c69febbc70ff65d
Kiril Tucker <kiril.tucker@gmail.com>
2022-05-04 16:42:27 +0200

    Provide + Inject for Functions / Methods

--- cmp-communication-08-a-potential-problem/src/App.vue
+++ cmp-communication-08-a-potential-problem/src/App.vue
@@ -4,7 +4,7 @@
       :topic-title="activeTopic && activeTopic.title"
       :text="activeTopic && activeTopic.fullText"
     ></active-element>
-    <knowledge-base @select-topic="activateTopic"></knowledge-base>
+    <knowledge-base></knowledge-base>
   </div>
 </template>
 
@@ -34,7 +34,8 @@ export default {
   },
   provide() {
     return {
-      topics: this.topics
+      topics: this.topics,
+      selectTopic: this.activateTopic
     }
   },
   methods: {
