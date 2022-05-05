4887b4458600e07bbe849390a63d2d4b33a559fc
Kiril Tucker <kiril.tucker@gmail.com>
2022-05-05 11:58:19 +0200

    Introducing Slots

--- cmp-adv-01/src/main.js
+++ cmp-adv-01/src/main.js
@@ -1,10 +1,12 @@
-import { createApp } from 'vue';
+import { createApp } from 'vue'
 
-import App from './App.vue';
-import BaseBadge from './components/BaseBadge.vue';
+import App from './App.vue'
+import BaseBadge from './components/BaseBadge.vue'
+import BaseCard from './components/BaseCard.vue'
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.component('base-badge', BaseBadge);
+app.component('base-badge', BaseBadge)
+app.component('base-card', BaseCard)
 
-app.mount('#app');
+app.mount('#app')
