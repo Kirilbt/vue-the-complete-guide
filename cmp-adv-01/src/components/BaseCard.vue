039dedeb3a1265e984ad5e5bef8e4fa9b94ebc5e
Kiril Tucker <kiril.tucker@gmail.com>
2022-05-05 14:02:31 +0200

    More on Slots

--- cmp-adv-01/src/components/BaseCard.vue
+++ cmp-adv-01/src/components/BaseCard.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-    <header>
-      <slot name="header"></slot>
+    <header v-if="$slots.header">
+      <slot name="header">
+        <!-- <h2>The Default</h2> -->
+      </slot>
     </header>
     <slot></slot>
   </div>
 </template>
 
 <script>
-  export default {}
+  export default {
+    mounted() {
+      console.log(this.$slots.header);
+    }
+  }
 </script>
 
 <style scoped>
