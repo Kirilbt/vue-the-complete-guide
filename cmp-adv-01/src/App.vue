6997e9148a3c06c201ece2a1cb3fe0923c8d2c8c
Kiril Tucker <kiril.tucker@gmail.com>
2022-05-05 14:25:03 +0200

    Scoped Slots

--- cmp-adv-01/src/App.vue
+++ cmp-adv-01/src/App.vue
@@ -8,18 +8,24 @@
       :role="activeUser.role"
     ></user-info>
   </div>
+  <course-goals #default="slotProps">
+    <h2>{{ slotProps.item }}</h2>
+    <p>{{ slotProps['another-prop'] }}</p>
+  </course-goals>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue'
 import BadgeList from './components/BadgeList.vue'
 import UserInfo from './components/UserInfo.vue'
+import CourseGoals from './components/CourseGoals.vue'
 
 export default {
   components: {
     TheHeader,
     BadgeList,
-    UserInfo
+    UserInfo,
+    CourseGoals
   },
   data() {
     return {
---
+++ cmp-adv-01/src/components/CourseGoals.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal">
+      <slot :item="goal" another-prop="Test"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        goals: ['Finish the Course', 'Learn Vue'],
+      }
+    }
+  }
+</script>
