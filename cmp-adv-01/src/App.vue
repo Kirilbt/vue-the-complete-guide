b49bfd5abba334107056cf106dd293aa221f5c11
Kiril Tucker <kiril.tucker@gmail.com>
2022-05-05 11:36:03 +0200

    Global vs Local Components

--- cmp-adv-01/src/App.vue
+++ cmp-adv-01/src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
+    <TheHeader />
+    <BadgeList />
+    <UserInfo
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
-    ></user-info>
+    />
   </div>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue'
+import BadgeList from './components/BadgeList.vue'
+import UserInfo from './components/UserInfo.vue'
+
 export default {
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo
+  },
   data() {
     return {
       activeUser: {
