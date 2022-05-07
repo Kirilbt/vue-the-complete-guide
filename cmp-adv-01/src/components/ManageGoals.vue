4c05bf0032735223f905dca139c26ecef0e15820
Kiril Tucker <kiril.tucker@gmail.com>
2022-05-07 12:50:52 +0200

    Applying What We Know & A Problem

--- cmp-adv-01/src/components/ManageGoals.vue
+++ cmp-adv-01/src/components/ManageGoals.vue
@@ -1,6 +1,38 @@
 <template>
   <div>
     <h2>Manage Goals</h2>
-    <input type="text">
+    <input type="text" ref="goal">
+    <button @click="setGoal">Set Goal</button>
+    <error-alert v-if="inputIsInvalid">
+      <h2>Input is invalid</h2>
+      <p>Input can't be empty!</p>
+      <button @click="confirmError">Okay</button>
+    </error-alert>
   </div>
 </template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+  export default {
+    components: {
+      ErrorAlert
+    },
+    data() {
+        return {
+          inputIsInvalid: false
+        };
+    },
+    methods: {
+      setGoal() {
+        const enteredValue = this.$refs.goal.value;
+        if (enteredValue === "") {
+            this.inputIsInvalid = true;
+        }
+      },
+      confirmError() {
+        this.inputIsInvalid = false
+      }
+    },
+    components: { ErrorAlert }
+}
+</script>
