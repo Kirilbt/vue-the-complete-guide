17bea255fb3ce098f6d0777bf283004fe6ec41a8
Kiril Tucker <kiril.tucker@gmail.com>
2022-05-07 13:17:25 +0200

    Teleporting Elements

--- cmp-adv-01/src/components/ManageGoals.vue
+++ cmp-adv-01/src/components/ManageGoals.vue
@@ -3,20 +3,19 @@
     <h2>Manage Goals</h2>
     <input type="text" ref="goal">
     <button @click="setGoal">Set Goal</button>
-    <error-alert v-if="inputIsInvalid">
-      <h2>Input is invalid</h2>
-      <p>Input can't be empty!</p>
-      <button @click="confirmError">Okay</button>
-    </error-alert>
+    <teleport to="body">
+      <error-alert v-if="inputIsInvalid">
+        <h2>Input is invalid</h2>
+        <p>Input can't be empty!</p>
+        <button @click="confirmError">Okay</button>
+      </error-alert>
+    </teleport>
   </div>
 </template>
 
 <script>
 import ErrorAlert from "./ErrorAlert.vue";
   export default {
-    components: {
-      ErrorAlert
-    },
     data() {
         return {
           inputIsInvalid: false
