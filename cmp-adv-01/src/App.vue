730d3ff2d7ba4ce55b654a92273a790cffe32b01
Kiril Tucker <kiril.tucker@gmail.com>
2022-05-07 12:22:39 +0200

    Keeping Dynamic Components Alive

--- cmp-adv-01/src/App.vue
+++ cmp-adv-01/src/App.vue
@@ -5,7 +5,9 @@
     <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
     <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
     <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
-    <component :is="selectedComponent"></component>
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
