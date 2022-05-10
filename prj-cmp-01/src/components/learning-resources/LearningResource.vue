9b4f763fd8e009760b75acfafd4e24228e582268
Kiril Tucker <kiril.tucker@gmail.com>
2022-05-10 16:12:00 +0200

    Adding a Base Button

--- prj-cmp-01/src/components/learning-resources/LearningResource.vue
+++ prj-cmp-01/src/components/learning-resources/LearningResource.vue
@@ -3,7 +3,7 @@
     <base-card>
       <header>
         <h3>{{ title }}</h3>
-        <button>Delete</button>
+        <base-button mode="flat">Delete</base-button>
       </header>
       <p>{{ description }}</p>
       <nav>
@@ -14,9 +14,11 @@
 </template>
 
 <script>
+import BaseButton from '../UI/BaseButton.vue';
   export default {
     props: ["title", "description", "link"],
-  }
+    components: { BaseButton }
+}
 </script>
 
 <style scoped>
