350bfdddc0c44fc22faa35cbe997f9cd34aae8b7
Kiril Tucker <kiril.tucker@gmail.com>
2022-05-10 16:00:53 +0200

    Header & BaseCard Components

--- prj-cmp-01/src/App.vue
+++ prj-cmp-01/src/App.vue
@@ -1,13 +1,16 @@
 <template>
+<the-header title="RememberMe"></the-header>
   <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
   import StoredResources from "./components/learning-resources/StoredResources.vue"
+  import TheHeader from "./components/layouts/TheHeader.vue"
 
   export default {
     components: {
-      StoredResources
+      StoredResources,
+      TheHeader
     },
     data() {
       return {
--- prj-cmp-01/src/components/learning-resources/LearningResource.vue
+++ prj-cmp-01/src/components/learning-resources/LearningResource.vue
@@ -1,21 +1,21 @@
 <template>
   <li>
-    <div>
+    <base-card>
       <header>
         <h3>{{ title }}</h3>
         <button>Delete</button>
       </header>
-    </div>
-    <p>{{ description }}</p>
-    <nav>
-      <a :href="link">View Resource</a>
-    </nav>
+      <p>{{ description }}</p>
+      <nav>
+        <a :href="link">View Resource</a>
+      </nav>
+    </base-card>
   </li>
 </template>
 
 <script>
   export default {
-    props: ['title', 'description', 'link']
+    props: ["title", "description", "link"],
   }
 </script>
 
