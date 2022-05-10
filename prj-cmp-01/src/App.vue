c72f442465958b60e30f28d8d579dddf7cbc679f
Kiril Tucker <kiril.tucker@gmail.com>
2022-05-10 16:44:27 +0200

    Dynamic Components & Attribute Fallthrough

--- prj-cmp-01/src/App.vue
+++ prj-cmp-01/src/App.vue
@@ -1,15 +1,12 @@
 <template>
-<the-header title="RememberMe"></the-header>
-  <stored-resources :resources="storedResources"></stored-resources>
+  <the-header title="RememberMe"></the-header>
 </template>
 
 <script>
-  import StoredResources from "./components/learning-resources/StoredResources.vue"
   import TheHeader from "./components/layouts/TheHeader.vue"
 
   export default {
     components: {
-      StoredResources,
       TheHeader
     },
     data() {
---
+++ prj-cmp-01/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,29 @@
+<template>
+  <base-card>
+    <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
+    <base-button @click="setSelectedTab('add-resource')">Add Resource</base-button>
+  </base-card>
+  <component :is="selectedTab"></component>
+</template>
+
+<script>
+  import StoredResources from "./StoredResources.vue"
+  import AddResource from "./AddResource.vue"
+
+  export default {
+    components: {
+      StoredResources,
+      AddResource
+    },
+    data() {
+      return {
+        selectedTab: 'stored-resources'
+      }
+    },
+    methods: {
+      setSelectedTab(tab) {
+        this.selectedTab = tab
+      }
+    }
+  }
+</script>
