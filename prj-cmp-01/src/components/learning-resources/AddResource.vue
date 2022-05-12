338b1d1cc45fde779cf8f90dcbdf50ad3693f992
Kiril Tucker <kiril.tucker@gmail.com>
2022-05-12 13:37:49 +0200

    Adding a Modal Dialog

--- prj-cmp-01/src/components/learning-resources/AddResource.vue
+++ prj-cmp-01/src/components/learning-resources/AddResource.vue
@@ -1,4 +1,13 @@
 <template>
+  <base-dialog v-if="inputIsInvalid" title="Invalid Input" @close="confirmError">
+    <template #default>
+      <p>Unfortunately, at least one input value is invalid.</p>
+      <p>Please check all inputs and make sure you enter a t least a few characters into each input fields.</p>
+    </template>
+    <template #actions>
+      <base-button @click="confirmError">Okay</base-button>
+    </template>
+  </base-dialog>
   <base-card>
     <form @submit.prevent="submitData">
       <div class="form-control">
@@ -23,13 +32,26 @@
 <script>
   export default {
     inject: ['addResource'],
+    data() {
+      return {
+        inputIsInvalid: false,
+      }
+    },
     methods: {
       submitData() {
         const enteredTitle = this.$refs.titleInput.value
         const enteredDesc = this.$refs.descInput.value
         const enteredLink = this.$refs.linkInput.value
 
+        if (enteredTitle.trim() === '' || enteredDesc.trim() === '' || enteredLink.trim() === '') {
+          this.inputIsInvalid = true
+          return
+        }
+
         this.addResource(enteredTitle, enteredDesc, enteredLink)
+      },
+      confirmError() {
+        this.inputIsInvalid = false
       }
     }
   }
--- prj-cmp-01/src/main.js
+++ prj-cmp-01/src/main.js
@@ -2,11 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import BaseCard from './components/UI/BaseCard.vue'
 import BaseButton from './components/UI/BaseButton.vue'
+import BaseDialog from './components/UI/BaseDialog.vue'
 
 const app = createApp(App)
 
 app.component('base-card', BaseCard)
    .component('base-button', BaseButton)
+   .component('base-dialog', BaseDialog)
 
 
 app.mount('#app')
