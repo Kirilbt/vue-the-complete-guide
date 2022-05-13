5fa1a91b175f2cd3b23a089dae355413c65086f5
Kiril Tucker <kiril.tucker@gmail.com>
2022-05-13 21:05:30 +0200

    Starting App & Why We Need a Backend

--- http-01/src/main.js
+++ http-01/src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+import BaseCard from './components/UI/BaseCard.vue';
+import BaseButton from './components/UI/BaseButton.vue';
+import App from './App.vue';
+
+const app = createApp(App);
+
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+
+app.mount('#app');
