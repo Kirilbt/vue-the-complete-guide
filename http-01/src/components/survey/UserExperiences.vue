f5caf399fc03d840acfb8615517fd220b30c1b5c
Kiril Tucker <kiril.tucker@gmail.com>
2022-05-14 07:30:49 +0200

    Getting Data (GET Request) & Transforming Response Data

--- http-01/src/components/survey/UserExperiences.vue
+++ http-01/src/components/survey/UserExperiences.vue
@@ -3,7 +3,7 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
       <ul>
         <survey-result
@@ -21,10 +21,34 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
   },
+  data() {
+    return {
+      results: []
+    }
+  },
+  methods: {
+    loadExperiences() {
+      fetch('https://vue-http-demo-7e9b0-default-rtdb.europe-west1.firebasedatabase.app/surveys.json').then((response) => {
+        if (response.ok) {
+          return response.json()
+        }
+      })
+      .then((data) => {
+        const results = []
+        for (const id in data) {
+          results.push({
+            id: id,
+            name: data[id].name,
+            rating: data[id].rating
+          })
+        }
+        this.results = results
+      })
+    }
+  }
 };
 </script>
 
