ecec66c9602ee3b43953a1275ccb4bf1898b4131
Kiril Tucker <kiril.tucker@gmail.com>
2022-05-14 07:51:37 +0200

    Handling the 'No Data' State

--- http-01/src/components/survey/UserExperiences.vue
+++ http-01/src/components/survey/UserExperiences.vue
@@ -6,7 +6,8 @@
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
       <p v-if="isLoading">Loading...</p>
-      <ul v-else>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">No stored experiences found. Start adding some results</p>
+      <ul v-else-if="!isLoading && results && results.length > 0">
         <survey-result
           v-for="result in results"
           :key="result.id"
