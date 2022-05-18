f9ea932b995ff9e8bdd49cfdce11bba23ebe112e
Kiril Tucker <kiril.tucker@gmail.com>
2022-05-18 16:36:48 +0200

    Updating Params Data with Watchers

--- routing-01/src/components/teams/TeamMembers.vue
+++ routing-01/src/components/teams/TeamMembers.vue
@@ -9,6 +9,7 @@
         :role="member.role"
       ></user-item>
     </ul>
+    <router-link to="/teams/t2">Go to Team 2</router-link>
   </section>
 </template>
 
@@ -27,18 +28,28 @@ export default {
       members: [],
     };
   },
+  methods: {
+    loadTeamMembers() {
+      const teamId = this.$route.params.teamId
+      const selectedTeam = this.teams.find(team => team.id === teamId)
+      const members = selectedTeam.members
+      const selectedMembers = []
+      for (const member of members) {
+        const selectedUser = this.users.find(user => user.id === member)
+        selectedMembers.push(selectedUser)
+      }
+      this.members = selectedMembers
+      this.teamName = selectedTeam.name
+    }
+  },
   created() {
     // this.$route.path // /teams/t1
-     const teamId = this.$route.params.teamId
-     const selectedTeam = this.teams.find(team => team.id === teamId)
-     const members = selectedTeam.members
-     const selectedMembers = []
-     for (const member of members) {
-       const selectedUser = this.users.find(user => user.id === member)
-       selectedMembers.push(selectedUser)
-     }
-     this.members = selectedMembers
-     this.teamName = selectedTeam.name
+    this.loadTeamMembers(this.$route)
+  },
+  watch: {
+    $route(newRoute) {
+      this.loadTeamMembers(newRoute)
+    }
   }
 };
 </script>
--- routing-01/src/components/teams/TeamsItem.vue
+++ routing-01/src/components/teams/TeamsItem.vue
@@ -2,13 +2,18 @@
   <li>
     <h3>{{ name }}</h3>
     <div class="team-members">{{ memberCount }} Members</div>
-    <router-link :to="'/teams/' + id">View Members</router-link>
+    <router-link :to="teamMembersLink">View Members</router-link>
   </li>
 </template>
 
 <script>
 export default {
   props: ['id', 'name', 'memberCount'],
+  computed: {
+    teamMembersLink() {
+      return '/teams/' + this.id
+    }
+  }
 };
 </script>
 
