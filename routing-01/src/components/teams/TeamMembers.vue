030fdb53cf95aea86561af37260c6b0b7d1ccd23
Kiril Tucker <kiril.tucker@gmail.com>
2022-05-18 16:49:40 +0200

    Passing Params as Props

--- routing-01/src/components/teams/TeamMembers.vue
+++ routing-01/src/components/teams/TeamMembers.vue
@@ -19,6 +19,7 @@ import UserItem from '../users/UserItem.vue';
 
 export default {
   inject: ['users', 'teams'],
+  props: ['teamId'],
   components: {
     UserItem
   },
@@ -29,8 +30,7 @@ export default {
     };
   },
   methods: {
-    loadTeamMembers() {
-      const teamId = this.$route.params.teamId
+    loadTeamMembers(teamId) {
       const selectedTeam = this.teams.find(team => team.id === teamId)
       const members = selectedTeam.members
       const selectedMembers = []
@@ -44,11 +44,11 @@ export default {
   },
   created() {
     // this.$route.path // /teams/t1
-    this.loadTeamMembers(this.$route)
+    this.loadTeamMembers(this.teamId)
   },
   watch: {
-    $route(newRoute) {
-      this.loadTeamMembers(newRoute)
+    teamId(newId) {
+      this.loadTeamMembers(newId)
     }
   }
 };
