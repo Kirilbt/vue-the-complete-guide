72dc8f1201ebb1298c36f7496900410e245c601d
Kiril Tucker <kiril.tucker@gmail.com>
2022-06-05 09:01:46 +0200

    Understanding CSS Animations

--- animations-01/src/App.vue
+++ animations-01/src/App.vue
@@ -63,7 +63,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
-  transition: transform .2s;
+  /* transition: transform .2s ease-out; */
 }
 .container {
   max-width: 40rem;
@@ -77,6 +77,21 @@ button:active {
   border-radius: 12px;
 }
 .animate {
-  transform: translateX(-150px);
+  /* transform: translateX(-150px); */
+  animation: slide-fade .3s ease-out forwards;
+}
+
+@keyframes slide-fade {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+
+  70% {
+    transform: translateX(-120px) scale(1.1);
+  }
+
+  100% {
+    transform: translateX(-150px) scale(1);
+  }
 }
 </style>
