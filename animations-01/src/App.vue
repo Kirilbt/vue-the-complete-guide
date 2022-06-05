a5fa9dfbc91820c0437cd23d4f03d3404de4c658
Kiril Tucker <kiril.tucker@gmail.com>
2022-06-05 14:34:25 +0200

    CSS Animations with the Transition Component

--- animations-01/src/App.vue
+++ animations-01/src/App.vue
@@ -88,10 +88,40 @@ button:active {
 }
 .animate {
   /* transform: translateX(-150px); */
-  animation: slide-fade .3s ease-out forwards;
+  animation: slide-scale .3s ease-out forwards;
 }
 
-@keyframes slide-fade {
+.v-enter-from {
+  /* opacity: 0;
+  transform: translateY(-30px); */
+}
+
+.v-enter-active {
+  /* transition: all .3s ease-out; */
+  animation: slide-scale .3s ease-out;
+}
+
+.v-enter-to {
+  /* opacity: 1;
+  transform: translateY(0); */
+}
+
+.v-leave-from {
+  /* opacity: 1;
+  transform: translateY(0); */
+}
+
+.v-leave-active {
+  /* transition: all .2s ease-in; */
+  animation: slide-scale .3s ease-out;
+}
+
+.v-leave-to {
+  /* opacity: 0;
+  transform: translateY(-30px); */
+}
+
+@keyframes slide-scale {
   0% {
     transform: translateX(0) scale(1);
   }
