56c6ee149854743c723eb9c8297ce9b6720205c5
Kiril Tucker <kiril.tucker@gmail.com>
2022-06-05 08:50:43 +0200

    Animation Basics & CSS Transitions

--- animations-01/src/App.vue
+++ animations-01/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
-    <div class="block"></div>
-    <button>Animate</button>
+    <div class="block" :class="{animate: animatedBlock}"></div>
+    <button @click="animateBlock">Animate</button>
   </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
@@ -15,9 +15,15 @@
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false };
+    return {
+        animatedBlock: false,
+        dialogIsVisible: false
+      };
   },
   methods: {
+    animateBlock() {
+      this.animatedBlock = true;
+    },
     showDialog() {
       this.dialogIsVisible = true;
     },
@@ -57,6 +63,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
+  transition: transform .2s;
 }
 .container {
   max-width: 40rem;
@@ -69,5 +76,7 @@ button:active {
   border: 2px solid #ccc;
   border-radius: 12px;
 }
-</style>
+.animate {
+  transform: translateX(-150px);
+}
 </style>
--- animations-01/src/main.js
+++ animations-01/src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+import BaseModal from './components/BaseModal.vue';
+
+const app = createApp(App);
+
+app.component('base-modal', BaseModal);
+
+app.mount('#app');
