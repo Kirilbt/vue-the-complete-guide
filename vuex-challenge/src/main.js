dd69b6539616985ee74d97a7097cce1131d4b8c6
Kiril Tucker <kiril.tucker@gmail.com>
2022-06-26 10:14:44 +0200

    Vuex Challenge - Solution 1/3

--- vuex-challenge/src/main.js
+++ vuex-challenge/src/main.js
@@ -1,13 +1,14 @@
-import { createApp } from 'vue';
+import { createApp } from 'vue'
 
-import router from './router.js';
-import App from './App.vue';
-import BaseBadge from './components/ui/BaseBadge.vue';
+import router from './router.js'
+import store from './store/index.js'
+import App from './App.vue'
+import BaseBadge from './components/ui/BaseBadge.vue'
 
 const app = createApp(App)
 
-app.use(router);
+app.use(router, store)
 
-app.component('base-badge', BaseBadge);
+app.component('base-badge', BaseBadge)
 
-app.mount('#app');
+app.mount('#app')
