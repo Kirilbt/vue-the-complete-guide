9c7c42dcca5dd7db565d9f6ac4a8b1dd20a3e46b
Kiril Tucker <kiril.tucker@gmail.com>
2022-06-26 15:30:24 +0200

    Vuex Challenge - Solution 3/3

--- vuex-challenge/src/App.vue
+++ vuex-challenge/src/App.vue
@@ -4,39 +4,25 @@
 </template>
 
 <script>
-import TheHeader from './components/nav/TheHeader.vue';
+import TheHeader from './components/nav/TheHeader.vue'
 
 export default {
   components: {
     TheHeader
-  },
-  data() {
-    return {
-      isLoggedIn: false,
-    }
-  },
-  methods: {
-
-    login() {
-      this.isLoggedIn = true;
-    },
-    logout() {
-      this.isLoggedIn = false;
-    },
-  },
-};
+  }
+}
 </script>
 
 <style>
 * {
-  box-sizing: border-box;
+  box-sizing: border-box
 }
 
 html {
-  font-family: sans-serif;
+  font-family: sans-serif
 }
 
 body {
-  margin: 0;
+  margin: 0
 }
 </style>
